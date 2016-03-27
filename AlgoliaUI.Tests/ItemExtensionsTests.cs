864a1597ce845ce8256cd8f4adaafbd8ac5102b0
Dmitry Harnitski <dmitry@brainjocks.com>
2016-03-27 17:44:33 -0400

    Refactor Refinement List to use datasource

---
+++ AlgoliaUI.Tests/ItemExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Sitecore.FakeDb;
+using AlgoliaUI.Code;
+using FluentAssertions;
+
+namespace AlgoliaUI.Tests
+{
+    [TestFixture]
+    public class ItemExtensionsTests
+    {
+        [Test]
+        public void ShouldDecodeField()
+        {
+            using (var db = new Db
+            {
+                new DbItem("Home") {{"Title", @"'&lt;div class=""facet - title""&gt;Materials&lt;/div class=""facet - title""&gt;'" } }
+            })
+            {
+                var home = db.GetItem("/sitecore/content/home");
+
+                var expected = home.GetFieldDecodedRawValue("Title");
+                expected.ToHtmlString().Should().Be(@"'<div class=""facet - title"">Materials</div class=""facet - title"">'");
+            }
+        }
+    }
+}
---
+++ AlgoliaUI/Code/ItemExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+
+namespace AlgoliaUI.Code
+{
+    public static class ItemExtensions
+    {
+        public static IHtmlString GetFieldDecodedRawValue(this Item item, string fieldName)
+        {
+            var value = item.Fields[fieldName].Value;
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+            var decodedValue = HttpUtility.HtmlDecode(value);
+
+            return new HtmlString(decodedValue);
+        }
+    }
+}
