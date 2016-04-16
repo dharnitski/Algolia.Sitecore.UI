e75b46f6d649ee63fdc0f3d5d629a59801b884c2
Dmitry Harnitski <dharnitski@gmail.com>
2016-04-15 22:08:30 -0400

    Create model for Refinement List

---
+++ AlgoliaUI.Tests/Models/RefinementListModelTests.cs
@@ -0,0 +1,62 @@
+﻿using AlgoliaUI.Code.Models;
+using FluentAssertions;
+using NUnit.Framework;
+using Sitecore.FakeDb;
+using Sitecore.Mvc.Presentation;
+
+namespace AlgoliaUI.Tests.Models
+{
+    [TestFixture]
+    public class RefinementListModelTests
+    {
+        [TestCase("", "")]
+        [TestCase(@"'&lt;div class=""facet - title""&gt;Materials&lt;/div class=""facet - title""&gt;'",
+            "header: '<div class=\"facet - title\">Materials</div class=\"facet - title\">',")]
+        public void HeaderTemplateTest(string input, string expected)
+        {
+            using (var db = new Db
+            {
+                new DbItem("home")
+                {
+                    {"Header Template", input}
+                }
+            })
+            {
+                var rendering = new Rendering
+                {
+                    Item = db.GetItem("/sitecore/content/home")
+                };
+
+                var sut = new RefinementListModel {Rendering = rendering};
+
+                var actual = sut.HeaderTemplate.ToString();
+                actual.Should().Be(expected);
+            }
+        }
+
+        [TestCase("", "")]
+        [TestCase(@"'&lt;div class=""facet - title""&gt;Materials&lt;/div class=""facet - title""&gt;'",
+            "item: '<div class=\"facet - title\">Materials</div class=\"facet - title\">',")]
+        public void ItemTemplateTest(string input, string expected)
+        {
+            using (var db = new Db
+            {
+                new DbItem("home")
+                {
+                    {"Item Template", input}
+                }
+            })
+            {
+                var rendering = new Rendering
+                {
+                    Item = db.GetItem("/sitecore/content/home")
+                };
+
+                var sut = new RefinementListModel { Rendering = rendering };
+
+                var actual = sut.ItemTemplate.ToString();
+                actual.Should().Be(expected);
+            }
+        }
+    }
+}
---
+++ AlgoliaUI/Code/Models/RefinementListModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using Sitecore.Mvc.Presentation;
+
+namespace AlgoliaUI.Code.Models
+{
+    public class RefinementListModel: RenderingModel
+    {
+        public IHtmlString ItemTemplate
+        {
+            get
+            {
+                return GetPropertyValue("item", "Item Template", GetFieldDecodedRawValue);
+            }
+        }
+
+        public IHtmlString HeaderTemplate
+        {
+            get
+            {
+                return GetPropertyValue("header", "Header Template", GetFieldDecodedRawValue);
+            }
+        }
+
+        private IHtmlString GetPropertyValue(string jsFieldName, string fieldName, Func<string, string> getFieldvalue)
+        {
+            if (string.IsNullOrEmpty(Item[fieldName]))
+                return new HtmlString(string.Empty);
+
+            return new HtmlString($"{jsFieldName}: {getFieldvalue(fieldName)},");
+        }
+
+        public string GetFieldDecodedRawValue(string fieldName)
+        {
+            var value = Item.Fields[fieldName].Value;
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+            var decodedValue = HttpUtility.HtmlDecode(value);
+
+            return decodedValue;
+        }
+    }
+}
