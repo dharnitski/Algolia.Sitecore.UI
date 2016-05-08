0ef9e75ff20f578130ea2065a341ed0519f418ea
Dmitry Harnitski <dharnitski@gmail.com>
2016-05-07 23:32:58 -0400

    refactor Refinement List Model add AlgoliaRenderingModel

---
+++ AlgoliaUI/Code/Models/AlgoliaRenderingModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Mvc.Presentation;
+
+namespace AlgoliaUI.Code.Models
+{
+    public abstract class AlgoliaRenderingModel: RenderingModel
+    {
+        /// <summary>
+        /// renders JS property in format: name: fieldValue, 
+        /// </summary>
+        /// <param name="jsFieldName"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="getFieldvalue"></param>
+        /// <returns></returns>
+        protected IHtmlString GetPropertyValue(string jsFieldName, string fieldName, Func<string, string> getFieldvalue)
+        {
+            if (string.IsNullOrEmpty(Item[fieldName]))
+                return new HtmlString(string.Empty);
+
+            return new HtmlString($"{jsFieldName}: {getFieldvalue(fieldName)},");
+        }
+
+        /// <summary>
+        /// makes sure that value is correct JS string. Could be 'value' or "value"
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        protected string GetStringFieldValue(string fieldName)
+        {
+            var value = GetFieldValue(fieldName);
+            if (value == null)
+                return null;
+
+            if (value.StartsWith(@"""") || value.StartsWith("'"))
+                return value;
+
+            value = $"'{value}'";
+
+            return value;
+        }
+
+        protected string GetFieldValue(string fieldName)
+        {
+            var value = Item.Fields[fieldName].Value;
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+            return value;
+        }
+
+        protected IHtmlString ReferenceKey(string fieldName, string defaultValue)
+        {
+            return Item.ReferenceKey(fieldName, defaultValue);
+        }
+
+        
+    }
+}
--- AlgoliaUI/Code/Models/RefinementListModel.cs
+++ AlgoliaUI/Code/Models/RefinementListModel.cs
@@ -5,75 +5,16 @@ using Sitecore.Mvc.Presentation;
 
 namespace AlgoliaUI.Code.Models
 {
-    public class RefinementListModel: RenderingModel
+    public class RefinementListModel: AlgoliaRenderingModel
     {
-        public IHtmlString ItemTemplate
-        {
-            get
-            {
-                return GetPropertyValue("item", "Item Template", GetStringFieldValue);
-            }
-        }
+        private const string DefaultOperator = "or";
 
-        private const string DefaultOperator = "or"; 
+        public IHtmlString ItemTemplate => GetPropertyValue("item", "Item Template", GetStringFieldValue);
 
-        public IHtmlString Operator
-        {
-            get
-            {
-                return Item.ReferenceKey("Operator", DefaultOperator);
-            } 
-        }
+        public IHtmlString Operator => ReferenceKey("Operator", DefaultOperator);
 
-        public IHtmlString HeaderTemplate
-        {
-            get
-            {
-                return GetPropertyValue("header", "Header Template", GetStringFieldValue);
-            }
-        }
+        public IHtmlString Limit => GetPropertyValue("limit", "Limit", GetFieldValue);
 
-        /// <summary>
-        /// renders JS property in format: name: fieldValue, 
-        /// </summary>
-        /// <param name="jsFieldName"></param>
-        /// <param name="fieldName"></param>
-        /// <param name="getFieldvalue"></param>
-        /// <returns></returns>
-        private IHtmlString GetPropertyValue(string jsFieldName, string fieldName, Func<string, string> getFieldvalue)
-        {
-            if (string.IsNullOrEmpty(Item[fieldName]))
-                return new HtmlString(string.Empty);
-
-            return new HtmlString($"{jsFieldName}: {getFieldvalue(fieldName)},");
-        }
-
-
-        /// <summary>
-        /// makes sure that value is correct JS string. Could be 'value' or "value"
-        /// </summary>
-        /// <param name="fieldName"></param>
-        /// <returns></returns>
-        private string GetStringFieldValue(string fieldName)
-        {
-            var value = GetFieldValue(fieldName);
-            if (value == null)
-                return null;
-
-            if (value.StartsWith(@"""") || value.StartsWith("'"))
-                return value;
-
-            value = $"'{value}'";
-
-            return value;
-        }
-
-        private string GetFieldValue(string fieldName)
-        {
-            var value = Item.Fields[fieldName].Value;
-            if (string.IsNullOrWhiteSpace(value))
-                return null;
-            return value;
-        }
+        public IHtmlString HeaderTemplate => GetPropertyValue("header", "Header Template", GetStringFieldValue);
     }
 }
